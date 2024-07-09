9728a1f218a1f75d46a73c039e9678232082c0d1
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-07-10 00:40:00 +0530

    Destructuring/Unpacking - Dictionaries

--- Py_Refesher.py
+++ Py_Refesher.py
@@ -1,21 +1,9 @@
-magic_number = 12 
-
-while True: 
-
-    user_input = input('Do you want to play the game?').lower()
-
-    if user_input == 'n':
-        break
-
-    number = int(input('Enter the number to crack the game!'))
-
-    if number == magic_number:
-        print('You got it! Congrats')
-    
-    elif abs(magic_number - number) == 1:
-        print('You were off by one. Try again, you are very close')
-
-    else: 
-        print('OOPS! Not good')
+mydict = {"booster": 28, "pandu" : 20, "chittt": 55}
 
 
+mylist = list(mydict.items())
+print(mylist)
+
+
+for name,age in mydict.items():
+    print(f'{name} age is {age}')

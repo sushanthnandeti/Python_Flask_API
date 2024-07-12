db4ecaca768a2a7fc4ef4bc9481b8f6456f9bec7
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-07-12 23:35:02 +0530

    keyword arguments unpacking

--- arguments_unpacking.py
+++ arguments_unpacking.py
@@ -1,24 +1,42 @@
-def multiply(*args):
+# def multiply(*args):
 
-    total = 1 
+#     total = 1 
 
-    for arg in args: 
-        total = total * arg
+#     for arg in args: 
+#         total = total * arg
 
-    return total
+#     return total
 
 
 
-def apply(*args, operator):
+# def apply(*args, operator):
 
-    if operator == "+":
-        return sum(args)
+#     if operator == "+":
+#         return sum(args)
     
-    elif operator == "*":
-        return multiply(*args)  #mandatory to use * here, as it unpacks the tuple and passes it on to the multiply function 
+#     elif operator == "*":
+#         return multiply(*args)  #mandatory to use * here, as it unpacks the tuple and passes it on to the multiply function 
     
-    else:
-        print("No luck!")
+#     else:
+#         print("No luck!")
 
 
-print(apply(1,3,5,operator="*"))
+# print(apply(1,3,5,operator="*"))
+
+
+
+def print_kwargs(**kwargs):
+    print(kwargs)
+
+
+
+def print_nicely(**kwargs):
+
+    print_kwargs(**kwargs)
+
+    for arg,value in kwargs.items():
+        print(f'{arg}: {value}')
+
+
+
+print_nicely(name="booster",age=25)
